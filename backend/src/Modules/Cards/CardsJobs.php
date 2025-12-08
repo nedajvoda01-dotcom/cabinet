0023cca2e8fae3714881e3aa164d13064c6fe06e
nedajvoda01-dotcom <nedajvoda01@icloud.com>
2025-12-08 23:00:59 +0300

    Update CardsJobs.php

--- backend/src/Modules/Cards/CardsJobs.php
+++ backend/src/Modules/Cards/CardsJobs.php
@@ -1,7 +1,35 @@
 <?php
-namespace Modules\Cards;
+declare(strict_types=1);
 
-class CardsJobs
+namespace Backend\Modules\Cards;
+
+/**
+ * CardsJobs
+ *
+ * Фоновые задачи Cards-домена:
+ *  - ручные ретраи карточек
+ *  - реиндексация/пересборка при необходимости
+ */
+final class CardsJobs
 {
-    // TODO: enqueue async jobs via Queues
+    public function __construct(
+        // TODO: инжект вашего QueueBus / WorkerDispatcher
+        // private QueueBus $bus
+    ) {}
+
+    public function dispatchCardRetry(int $cardId, string $reason, bool $force): void
+    {
+        // TODO: заменить на реальную очередь
+        // $this->bus->push('cards.retry', [
+        //     'card_id' => $cardId,
+        //     'reason' => $reason,
+        //     'force' => $force,
+        // ]);
+    }
+
+    public function dispatchReindexCard(int $cardId): void
+    {
+        // TODO optional
+        // $this->bus->push('cards.reindex', ['card_id' => $cardId]);
+    }
 }
