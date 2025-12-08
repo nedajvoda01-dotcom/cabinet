072326a126dd6ff63e1b8d43475b6d0669c59c5c
nedajvoda01-dotcom <nedajvoda01@icloud.com>
2025-12-08 18:58:31 +0300

    Delete microservices directory

--- microservices/auth/authService.test.js
+++
@@ -1,8 +0,0 @@
-// microservices/auth/authService.js
-const jwt = require('jsonwebtoken');
-
-const generateToken = (user) => {
-  return jwt.sign({ id: user.id, name: user.name }, 'secretKey', { expiresIn: '1h' });
-};
-
-module.exports = { generateToken };
