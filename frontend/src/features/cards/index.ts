8656890925d3ba3486d9b66826436d2d02788f72
nedajvoda01-dotcom <nedajvoda01@icloud.com>
2025-12-08 23:29:28 +0300

    Update index.ts

--- frontend/src/features/cards/index.ts
+++ frontend/src/features/cards/index.ts
@@ -1,4 +1,5 @@
-export * from "./api";
-export * from "./model";
+// cabinet/frontend/src/features/cards/index.ts
 export * from "./schemas";
+export * from "./model";
+export * from "./api";
 export * as CardsUI from "./ui";
