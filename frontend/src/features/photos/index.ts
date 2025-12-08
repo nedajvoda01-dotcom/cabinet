759032d0a363638c252b2bb47254d36ee9accf71
nedajvoda01-dotcom <nedajvoda01@icloud.com>
2025-12-08 23:37:32 +0300

    Update index.ts

--- frontend/src/features/photos/index.ts
+++ frontend/src/features/photos/index.ts
@@ -1,4 +1,5 @@
-export * from "./api";
-export * from "./model";
+// frontend/src/features/photos/index.ts
 export * from "./schemas";
+export * from "./model";
+export * from "./api";
 export * as PhotosUI from "./ui";
