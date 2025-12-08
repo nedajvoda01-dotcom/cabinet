77f0a7d4e40cfb68c8a5ad8cd9b611a1062bc630
nedajvoda01-dotcom <nedajvoda01@icloud.com>
2025-12-08 18:58:58 +0300

    Delete frontend directory

--- frontend/src/buttons/Button.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/components/buttons/Button.js
-import React from 'react';
-
-const Button = ({ label, onClick }) => {
-  return <button onClick={onClick}>{label}</button>;
-};
-
-export default Button;
