b41afeab18dba44d01bc6a920f352c88bc2d380f
nedajvoda01-dotcom <nedajvoda01@icloud.com>
2025-12-08 23:26:03 +0300

    Update index.ts

--- frontend/src/features/admin/index.ts
+++ frontend/src/features/admin/index.ts
@@ -1,4 +1,5 @@
-export * from "./api";
-export * from "./model";
+// cabinet/frontend/src/features/admin/index.ts
 export * from "./schemas";
+export * from "./model";
+export * from "./api";
 export * as AdminUI from "./ui";
