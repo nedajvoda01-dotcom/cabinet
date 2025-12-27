4a2239c17d6c442eb8dd6fa340dbcbad95015852
nedajvoda01-dotcom <nedajvoda01@icloud.com>
2025-12-27 21:35:22 +0300

    Delete frontend/src/apps/admin directory

--- frontend/src/apps/admin/index.tsx
+++
@@ -1,7 +0,0 @@
-// cabinet/frontend/src/apps/admin/index.tsx
-import React from "react";
-import { AdminRoutes } from "./routes";
-
-export default function AdminApp() {
-  return <AdminRoutes />;
-}
